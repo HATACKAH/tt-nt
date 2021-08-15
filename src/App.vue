a10c2311b5c3ed4a3abf1e2a11f2134361d1d688
Hleb <godads@yandex.ru>
2021-08-15 17:37:42 +0300

    first ver test project

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,47 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <div v-for="n in notifications" :key="n.title">
+            <PushAlert v-if="n.type == 'push'" :notification="n" />
+        </div>
     </div>
-    <router-view/>
-  </div>
 </template>
 
+<script>
+import PushAlert from './components/PushAlert.vue';
+
+export default {
+    data() {
+        return {
+            notifications: [
+                {
+                    title: 'title1',
+                    description: 'Description 1',
+                    type: 'push',
+                },
+                {
+                    title: 'title2',
+                    description: 'Description 3',
+                    type: 'push',
+                },
+                {
+                    title: 'title3',
+                    description: 'Description 3',
+                    type: 'modal',
+                },
+            ],
+        };
+    },
+    components: {
+        PushAlert,
+    },
+};
+</script>
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
     color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
---
+++ src/components/PushAlert.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="push">
+        <h3>{{ notification.title }}</h3>
+        <p>{{ notification.description }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PushAlert',
+
+    props: {
+        notification: {
+            type: Object,
+            required: true,
+        },
+    },
+};
+</script>
+
+<style scoped lang="scss">
+@keyframes opacityon {
+    from {
+        opacity: 0;
+    }
+    to {
+        opacity: 1;
+    }
+}
+.push {
+    width: 20%;
+    margin: auto;
+    border: 1px solid #b48af2;
+    border-radius: 10px;
+    margin-bottom: 10px;
+    animation-duration: 1.5s;
+    animation-name: opacityon;
+}
+
+@media screen and (max-width: 700px) {
+    .push {
+        width: 100%;
+    }
+}
+</style>
